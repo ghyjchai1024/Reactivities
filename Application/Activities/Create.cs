faaeb542dfaa90eeb3cf84315c6ccfb1e5635823
ghyjchai1024 <ron.yongjie@gmail.com>
2021-02-25 19:52:12 +0800

    End of Section 14

--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -5,6 +5,8 @@ using Domain;
 using MediatR;
 using Persistence;
 using FluentValidation;
+using Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.Activities
 {
@@ -37,17 +39,19 @@ namespace Application.Activities
         public class Handler : IRequestHandler<Command>
         {
             private readonly DataContext _context;
-            public Handler(DataContext context)
+            private readonly IUserAccessor _userAccessor;
+            public Handler(DataContext context, IUserAccessor userAccessor)
             {
+                _userAccessor = userAccessor;
                 _context = context;
             }
 
-            public async Task<Unit> Handle(Command request, 
+            public async Task<Unit> Handle(Command request,
                 CancellationToken cancellationToken)
             {
                 var activity = new Activity
                 {
-                     Id = request.Id,
+                    Id = request.Id,
                     Title = request.Title,
                     Description = request.Description,
                     Category = request.Category,
@@ -57,9 +61,23 @@ namespace Application.Activities
                 };
 
                 _context.Activities.Add(activity);
+
+                var user = await _context.Users.SingleOrDefaultAsync(x =>
+                    x.UserName == _userAccessor.GetCurrentUsername());
+
+                var attendee = new UserActivity
+                {
+                    AppUser = user,
+                    Activity = activity,
+                    IsHost = true,
+                    DateJoined = DateTime.Now,
+                };
+
+                _context.UserActivities.Add(attendee);
+
                 var success = await _context.SaveChangesAsync() > 0;
 
-                if(success) return Unit.Value;
+                if (success) return Unit.Value;
 
                 throw new Exception("Problem saving changes");
             }
